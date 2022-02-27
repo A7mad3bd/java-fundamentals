ad4ec3e5fda5e17c8990b9a7693f455ec507b3f8
A7mad3bd <ahmadabd398@gmail.com>
2022-02-27 20:54:25 +0300

    edited

--- basics/src/Main.java
+++ basics/src/Main.java
@@ -13,19 +13,14 @@ public class Main {
         int turtleCount = 0;
         System.out.println("I own " + turtleCount + " " + pluralize("turtle", turtleCount) + ".");
 
-
         flipNHeads(2);
-
-
-        //clock();
-
-
+        clock();
     }
 
     // Functions that return nothing have a return type of "void"
     public static String pluralize(String st, int count) {
-        if (count <= 1) {
 
+        if (count == 1) {
             return st;
         } else {
             st = st + "s";
@@ -35,51 +30,49 @@ public class Main {
 
     public static String flipNHeads(int n) {
         int chead = 0;
-        String s1=new String();
+        String s1 = new String();
         boolean flag;
-        String s2=new String();
-        int count=0;
-        while(chead!=n)
-        {
+        String s2 = new String();
+        int count = 0;
+        while (chead != n) {
             count++;
 
-            double M=Math.random();
+            double M = Math.random();
 
-            if ( M < .5)
-                flag=false;
+            if (M < .5)
+                flag = false;
             else
-                flag=true;
-            if(flag==false) {
+                flag = true;
+            if (flag == false) {
                 System.out.println("tails");
                 chead = 0;
-            }
-            else {
+            } else {
                 System.out.println("heads");
 
                 chead++;
             }
-            if(chead==n)
+            if (chead == n)
                 System.out.println("It took " + count + " flips to flip " + n + " head in a row.");
-
-
-
-
         }
-        return s1 ;
-
+        return s1;
     }
 
 
-    public static String clock()
-    {
-        for (int x=0;x<=1;) {
-            LocalDateTime now = LocalDateTime.now();
-            int hour = now.getHour();
-            int minute = now.getMinute();
-            int second = now.getSecond();
-            String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
-            System.out.println(time);
+    public static void clock() {
+
+        LocalDateTime now = LocalDateTime.now();
+        int this_sec = now.getSecond();
+        while (true) {
+            now = LocalDateTime.now();
+            int specific_esc = now.getSecond();
+            if (this_sec != specific_esc) {
+                int sec = now.getSecond();
+                int min = now.getMinute();
+                int hour = now.getHour();
+                System.out.println(hour + ":" + min + ":" + sec);
+                this_sec = now.getSecond();
+            }
         }
-        return null;
     }
 }
+
